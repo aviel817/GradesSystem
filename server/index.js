01c22730e7cd861bdaa09c06bcb6e6119fd2be19
Aviel <aviel1440@gmail.com>
2022-12-28 11:59:23 +0200

    connection to db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+secrets = require('./.secrets')
+
+const dbURL = secrets.dbURL
 
 app.get('/api', (req, res) => {
     res.json({"users": ["user1", "user2", "user3"]})
@@ -9,3 +13,11 @@ app.get('/api', (req, res) => {
 app.listen(5000, ()=> {
     console.log("Server started on port 5000")
 })
+
+mongoose.connect(dbURL)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
