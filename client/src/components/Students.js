5e3fdfc1c230fe1017cc4e96b3f1dbe9ecd2ac01
Aviel <aviel1440@gmail.com>
2023-01-31 18:06:47 +0200

    add plus icon

--- client/src/components/Students.js
+++ client/src/components/Students.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Table from './Table';
+import { FaPlusCircle } from 'react-icons/fa';
 
 
 const StudentsList = () => {
@@ -9,8 +10,13 @@ const StudentsList = () => {
     const tblHeaders = ["#", "First Name", "Last Name", "ID", "Courses"]
     return (
         <div>
-            <h1>Students List</h1>
-            <Table data={students} headers={tblHeaders} />
+            <div>
+                <h1>Students List</h1>
+                <Table data={students} headers={tblHeaders} />
+            </div>
+            <div className='row g-4' style={{textAlign: 'right', marginRight: '250px'}}>
+                <h1><FaPlusCircle /></h1>
+            </div>
         </div>
     );
 }
